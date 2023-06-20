97689620c3c4fa3891c00e3a33e936edf5679574
sergio mancilla <mancilla@fktech.net>
2023-06-19 23:52:26 -0300

    upd3

--- services/cronjob.js
+++ services/cronjob.js
@@ -23,7 +23,7 @@ Cron.schedule('* * * * *', async () => {
       const idProfesional = paciente.recordatorios[i].idProfesional.toString()
       const profesional = await ProfesionalService.traerPorId(idProfesional)
       for (const hora in recordatorio) {
-        if (hora === "11:11") {
+        if (hora === horaActual) {
           console.log("HORA ACTUAL:", horaActual)
           const medicamentos = recordatorio[hora]
           
