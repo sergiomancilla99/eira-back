94255e8a19e69d1956e1c2a888d31e0020d2f2df
sergio mancilla <mancilla@fktech.net>
2023-07-27 18:37:09 -0300

    update6

--- services/cronjob.js
+++ services/cronjob.js
@@ -40,7 +40,7 @@ Cron.schedule('* * * * *', async () => {
               "notification": {
                 "title": "Eira",
                 "body": `Es hora de tomar ${medicamento.nombre}`,
-                "click_action": `https://eira.ar/paciente/confirmacion?idProfesional=${idProfesional}&nombreProfesional=${profesional.nombre}&apellidoProfesional=${profesional.apellido}&medicamento=${medicamento.nombre}&idTratamiento=${idTratamiento}`,
+                // "click_action": `https://eira.ar/paciente/confirmacion?idProfesional=${idProfesional}&nombreProfesional=${profesional.nombre}&apellidoProfesional=${profesional.apellido}&medicamento=${medicamento.nombre}&idTratamiento=${idTratamiento}`,
                 "icon": "https://eira.ar/eira-icon.png",
                 vibrate: [300, 100, 400]
               },          
