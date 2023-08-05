a319292b08f91f87f695761f25354fe4c7aec39c
Saphira1406 <mellone.flore@gmail.com>
2023-08-05 19:37:18 -0300

    Templates de mails

--- services/mail.service.js
+++ services/mail.service.js
@@ -1,35 +1,16 @@
 import nodemailer from 'nodemailer'
 import * as dotenv from 'dotenv'
 dotenv.config({ path: 'variables.env' })
+import hbs from 'nodemailer-express-handlebars';
+import path from 'path';
 
-async function enviarNotificacionMail(email) {
-    let transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
-        auth: {
-          user: process.env.MAIL_USER,
-          pass: process.env.MAIL_PASSWORD,
-        },
-        tls: {
-            rejectUnauthorized: false
-        }
-      });
+//const hbs = require('nodemailer-express-handlebars');
 
-      let info = await transporter.sendMail({
-        from: 'No olvides tomar tu medicamento <eiraappinformacion@gmail.com>',
-        to: email,
-        subject: "Recordatorio",
-        html: "<b>No olvides tomar tu medicamento IBUPROFENO 300mg a las 20.00hs</b>",
-      });
-
-      return info
-}
-
-async function enviarToken(email, token) {
+function crearTransporter() {
   let transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
+    secure: true,
     auth: {
       user: process.env.MAIL_USER,
       pass: process.env.MAIL_PASSWORD,
@@ -39,11 +20,55 @@ async function enviarToken(email, token) {
     }
   });
 
+  return transporter;
+}
+
+function configurarTranporter(transporter) {
+  transporter.use('compile', hbs({
+    viewEngine: {
+      extname: '.handlebars',
+      partialsDir: path.resolve('./../eira-back/templates'),
+      defaultLayout: false,
+    },
+    viewPath: path.resolve('./../eira-back/templates'),
+    extName: '.handlebars'
+  }))
+}
+
+async function enviarNotificacionMail(email) {
+  let transporter = crearTransporter();
+
+  configurarTranporter(transporter);
+
+    let info = await transporter.sendMail({
+      from: 'Eira <eiraappinformacion@gmail.com>',
+      to: email,
+      subject: "PRUEBA",
+      /*html: "<b>No olvides tomar tu medicamento IBUPROFENO 300mg a las 20.00hs</b>",*/
+      template: "prueba",
+      context: {
+        email: email
+      }
+    });
+
+    return info
+}
+
+async function enviarToken(email, token) {
+  let transporter = crearTransporter();
+
+  configurarTranporter(transporter);
+
   let info = await transporter.sendMail({
       from: '"Eira" <eiraappinformacion@gmail.com>',
       to: `${email}`,
       subject: "Recuperar Contraseña",
-      html: `Este es tu link para recuperar la contraseña. Tené en cuenta que una vez que lo uses no podrás reutilizarlo. <br/>Hace <a href="https://eira.ar/recuperarContrasena/${token}/${email}">click aquí</a> para cambiar la contraseña`,
+      template: "recuperar",
+      context: {
+        email: email,
+        token: token
+      },
+      // html: `Este es tu link para recuperar la contraseña. Tené en cuenta que una vez que lo uses no podrás reutilizarlo. <br/>Hace <a href="https://eira.ar/recuperarContrasena/${token}/${email}">click aquí</a> para cambiar la contraseña`,
   });
 
   console.log("Message sent: %s", info.messageId);
@@ -51,23 +76,16 @@ async function enviarToken(email, token) {
 }
 
 async function contrasenaRecuperada(email) {
-  let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    auth: {
-      user: process.env.MAIL_USER,
-      pass: process.env.MAIL_PASSWORD,
-    },
-    tls: {
-        rejectUnauthorized: false
-    }
-  });
+  let transporter = crearTransporter();
+
+  configurarTranporter(transporter);
 
   let info = await transporter.sendMail({
       from: '"Eira" <eiraappinformacion@gmail.com>',
       to: `${email}`,
       subject: "Tu contraseña fue cambiada",
-      html: `Tu contraseña fue cambiada exitosamente.<br/> Si vos no hiciste el cambio contactanos a eiraappinformacion@gmail.com`,
+      // html: `Tu contraseña fue cambiada exitosamente.<br/> Si vos no hiciste el cambio contactanos a eiraappinformacion@gmail.com`,
+      template: "recuperado",
   });
 
   console.log("Message sent: %s", info.messageId);
@@ -75,23 +93,21 @@ async function contrasenaRecuperada(email) {
 }
 
 async function avisoValidarMatricula(medico) {
-  let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    auth: {
-      user: process.env.MAIL_USER,
-       pass: process.env.MAIL_PASSWORD,
-    },
-    tls: {
-        rejectUnauthorized: false
-    }
-  });
+  let transporter = crearTransporter();
+
+  configurarTranporter(transporter);
 
   let info = await transporter.sendMail({
       from: '"Eira" <eiraappinformacion@gmail.com>',
       to: 'eiraappinformacion@gmail.com',
       subject: "Se registró un nuevo médico - Validar matrícula",
-      html: `Se registró el médico <b>${medico.nombre} ${medico.apellido}</b> con número de matrícula: <b>${medico.matricula}</b>.<br>Validar matrícula para que el médico pueda hacer uso de la app.`,
+      // html: `Se registró el médico <b>${medico.nombre} ${medico.apellido}</b> con número de matrícula: <b>${medico.matricula}</b>.<br>Validar matrícula para que el médico pueda hacer uso de la app.`,
+      template: "nuevomedico",
+      context: {
+        nombre: medico.nombre,
+        apellido: medico.apellido,
+        matricula: medico.matricula,
+      },
   });
 
   console.log("Message sent: %s", info.messageId);
@@ -99,35 +115,35 @@ async function avisoValidarMatricula(medico) {
 }
 
 async function avisoMedicoVerificacion(medico) {
-  let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    auth: {
-      user: process.env.MAIL_USER,
-      pass: process.env.MAIL_PASSWORD,
-    },
-    tls: {
-        rejectUnauthorized: false
-    }
-  });
+  let transporter = crearTransporter();
+
+  configurarTranporter(transporter);
 
   let asunto = null;
-  let texto = null;
+  // let texto = null;
+  let template = null;
 
   if(medico.verificado) {
     asunto = "Tu matrícula fue verificada"
-    texto = `Hola ${medico.nombre} ${medico.apellido}, ya verificamos tu matrícula, y te dimos acceso a las funcionalidades de profesionales de salud.`
+    // texto = `Hola ${medico.nombre} ${medico.apellido}, ya verificamos tu matrícula, y te dimos acceso a las funcionalidades de profesionales de salud.`
+    template = "verificado"
   } else {
     asunto = "Hubo un problema con la verificación de tu matrícula"
-    texto = `Hola ${medico.nombre} ${medico.apellido}, tuvimos problemas para poder verificar tu matrícula. Por favor, ponete en contacto en nosotros para poder resolver el problema lo antes posbile.<br>
-    Escirbinos un mail con el asunto "No se verificó mi matrícula" a <a href="mailto:eiraappinformacion@gmail.com" target="_blank">eiraappinformacion@gmail.com</a>`
+    // texto = `Hola ${medico.nombre} ${medico.apellido}, tuvimos problemas para poder verificar tu matrícula. Por favor, ponete en contacto en nosotros para poder resolver el problema lo antes posbile.<br>
+    // Escirbinos un mail con el asunto "No se verificó mi matrícula" a <a href="mailto:eiraappinformacion@gmail.com" target="_blank">eiraappinformacion@gmail.com</a>`
+    template = "noverificado"
   }
 
   let info = await transporter.sendMail({
       from: '"Eira" <eiraappinformacion@gmail.com>',
       to: medico.email,
       subject: asunto,
-      html: texto,
+      // html: texto,
+      template: template,
+      context: {
+        nombre: medico.nombre,
+        apellido: medico.apellido,
+      },
   });
 
   console.log("Message sent: %s", info.messageId);
@@ -135,23 +151,19 @@ async function avisoMedicoVerificacion(medico) {
 }
 
 async function pedidoReceta(email, paciente) {
-  let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    auth: {
-      user: process.env.MAIL_USER,
-       pass: process.env.MAIL_PASSWORD,
-    },
-    tls: {
-        rejectUnauthorized: false
-    }
-  });
+  let transporter = crearTransporter();
+
+  configurarTranporter(transporter);
 
   let info = await transporter.sendMail({
       from: '"Eira" <eiraappinformacion@gmail.com>',
       to: `${email}`,
       subject: "Pedido de Receta",
-      html: `El paciente <strong>${paciente}</strong> hizo un pedido de receta. Iniciá sesión y visitá la sección de recetas para poder ver el listado de pedidos.`,
+      // html: `El paciente <strong>${paciente}</strong> hizo un pedido de receta. Iniciá sesión y visitá la sección de recetas para poder ver el listado de pedidos.`,
+      template: "pedidoreceta",
+      context: {
+        paciente: paciente,
+      },
   });
 
   console.log("Message sent: %s", info.messageId);
@@ -159,23 +171,19 @@ async function pedidoReceta(email, paciente) {
 }
 
 async function envioReceta(email, nombre) {
-  let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    auth: {
-      user: process.env.MAIL_USER,
-       pass: process.env.MAIL_PASSWORD,
-    },
-    tls: {
-        rejectUnauthorized: false
-    }
-  });
+  let transporter = crearTransporter();
+
+  configurarTranporter(transporter);
 
   let info = await transporter.sendMail({
       from: '"Eira" <eiraappinformacion@gmail.com>',
       to: `${email}`,
       subject: "Envío de Receta",
-      html: `¡Hola ${nombre}! El médico cargó la receta que pediste. Iniciá sesión y visitá la sección de recetas para poder verla.`,
+      // html: `¡Hola ${nombre}! El médico cargó la receta que pediste. Iniciá sesión y visitá la sección de recetas para poder verla.`,
+      template: "envioreceta",
+      context: {
+        nombre: nombre,
+      },
   });
 
   console.log("Message sent: %s", info.messageId);
@@ -183,23 +191,21 @@ async function envioReceta(email, nombre) {
 }
 
 async function contactoWeb(email, nombre, mensaje) {
-  let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    auth: {
-      user: process.env.MAIL_USER,
-       pass: process.env.MAIL_PASSWORD,
-    },
-    tls: {
-        rejectUnauthorized: false
-    }
-  });
+  let transporter = crearTransporter();
+
+  configurarTranporter(transporter);
 
   let info = await transporter.sendMail({
       from: '"Eira" <eiraappinformacion@gmail.com>',
       to: "eiraappinformacion@gmail.com",
       subject: "Contacto desde la web",
-      html: `<strong>${nombre}</strong> se contactó desde la web.<br/>Email: <strong>${email}</strong><br/> Mensaje:<br/>${mensaje}`,
+      // html: `<strong>${nombre}</strong> se contactó desde la web.<br/>Email: <strong>${email}</strong><br/> Mensaje:<br/>${mensaje}`,
+      template: "contacto",
+      context: {
+        nombre: nombre,
+        email: email,
+        mensaje: mensaje,
+      },
   });
 
   console.log("Message sent: %s", info.messageId);
